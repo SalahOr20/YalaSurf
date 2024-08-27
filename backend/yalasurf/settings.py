f8f8c273b0ad4891da54223004999f3c29a653f8
Salahor <salahor20@gmail.com>
2024-08-27 21:47:06 +0200

    Setting V2

--- backend/yalasurf/settings.py
+++ backend/yalasurf/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'rest_framework_simplejwt',
+
 ]
 
 
